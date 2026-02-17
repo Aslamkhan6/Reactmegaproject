9abc1d12b15f4d4f53e4ba264d74a87286e1eb21
Aslamkhan6 <akkhan7862345@gmail.com>
2026-02-17 23:25:07 +0500

    component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import React from "react"
-
-
-
+import { useDispatch } from "react-redux";
+import { useState,useEffect } from "react";
+import authService from "./appwrite/auth";
+import {login,logout} from "./store/Authslice";
 function App() {
+ const [loading, setloading] = useState(true);
+const dispatch = useDispatch();
+useEffect(() => {
+ authService.getCurrentUser().then((data)=>{
+  if(data){
+    dispatch(login({data}))
+  }
+  else{
+    dispatch(logout())
+  }
+ }).finally(()=>{
+  setloading(false)
+ })
  
+}, [])
 
   return (
     <>
